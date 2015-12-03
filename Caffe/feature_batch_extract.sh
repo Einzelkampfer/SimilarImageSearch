d254de0b39cddaab72399537afe2aaca2498beec
zsz <zhengszh3@gmail.com>
2015-12-03 22:30:06 +0800

    upload script

---
+++ Caffe/feature_batch_extract.sh
@@ -0,0 +1,22 @@
+linenum=$(cat features.txt| wc -l)
+start=$(date "+%s")
+i=0
+cat trainImgList.txt  | while read myline
+do
+	let "i++"
+	if [ "$i" -gt "$linenum" ];then
+		s=$(date "+%s")
+		./build/tools/googlenet_extract_feature.bin \
+		models/bvlc_googlenet/deploy.prototxt \
+		models/bvlc_googlenet/bvlc_googlenet.caffemodel \
+		data/ilsvrc12/imagenet_mean.binaryproto \
+		/media/zsz/新加卷/features.txt \
+		pool5/7x7_s1 $myline
+		e=$(date "+%s")
+		time=$((e-s))
+		echo "time used:$time seconds"
+	fi
+done
+end=$(date "+%s")
+time=$((end-start))
+echo "total time used:$time seconds"
